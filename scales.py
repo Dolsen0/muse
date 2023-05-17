2e72dd335db792f5800a5a58e78fa5a01e9d58a0
dolsen0 <derek.olsen@gmail.com>
2023-05-17 14:13:14 -0400

    refactored code and made output easier to read

--- scales.py
+++ scales.py
@@ -1,7 +1,7 @@
-chromatic = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "c"]
+CHROMATIC_SCALE = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"]
 
-interval_names = {
-    0: "Root",
+INTERVAL_NAMES = {
+    0: "R",
     1: "m2",
     2: "M2",
     3: "m3",
@@ -16,29 +16,28 @@ interval_names = {
     12: "P8"
 }
 
-major = [0, 2, 4, 5, 7, 9, 11, 12]
+MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11, 12]
 
-minor = [0, 2, 3, 5, 7, 8, 10, 12]
+MINOR_SCALE = [0, 2, 3, 5, 7, 8, 10, 12]
 
-diminished = [0, 2, 3, 5, 6, 8, 9, 11, 12]
+DIMINISHED_SCALE = [0, 2, 3, 5, 6, 8, 9, 11, 12]
 
-augmented = [0, 2, 4, 6, 8, 10, 12]
+AUGMENTED_SCALE = [0, 2, 4, 6, 8, 10, 12]
 
 def get_chromatic_scale(root):
-    root_index = chromatic.index(root.lower())
-    return f"\n {chromatic[root_index:] + chromatic[:root_index + 1]}"
+    root_index = CHROMATIC_SCALE.index(root.lower())
+    return f"\n {CHROMATIC_SCALE[root_index:] + CHROMATIC_SCALE[:root_index + 1]}"
 
 def get_scale(root, scale_name):
-    root_index = chromatic.index(root.lower())
-    scale_list = globals().get(scale_name.lower())
+    root_index = CHROMATIC_SCALE.index(root.lower())
+    scale_list = globals().get(scale_name.upper() + "_SCALE")
     if scale_list is None:
         return None
-    intervals_str = "-".join(interval_names[scale_list[i]] for i in range(len(scale_list)))
-    notes_str = "-".join(chromatic[(root_index + scale_list[i]) % 12] for i in range(len(scale_list)))
-    return f"\n{root.capitalize()} {scale_name.capitalize()}: {notes_str} ({intervals_str})"
+    notes_str = " _ ".join(CHROMATIC_SCALE[(root_index + scale_list[i]) % 12] for i in range(len(scale_list)))
+    intervals_str = " _ ".join(INTERVAL_NAMES[scale_list[i]] for i in range(len(scale_list)))
+    return f"\n{root.capitalize()} {scale_name.capitalize()}: \n({notes_str}) \n({intervals_str})\n"
 
 print(get_scale("E", "major"))
-
 print(get_scale("E", "minor"))
-
-print(get_chromatic_scale("E"))
+print(get_scale("E", "diminished"))
+print(get_scale("E", "augmented"))
